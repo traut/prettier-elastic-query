570a8a737bd61807e23920802ec7e4aefff7aaa5
Sergey Polzunov <sergey@polzunov.com>
2018-08-16 15:34:38 +0200

    make runner smarter

--- runner.js
+++ runner.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var pretty = require('./prettier-es')
 
+var ONLY_ERRORS = false;
+
 if (process.argv.length != 3 && process.argv.length != 4) {
     console.info("Usage: node runner.js <width> <query>")
     console.info("Usage: node runner.js <file>")
@@ -19,8 +21,28 @@ if (process.argv.length == 4) {
     var filename = process.argv[2];
     var lines = fs.readFileSync(filename);
     var width = 80;
+
+    var failures_count = 0;
+    var runs_count = 0;
     lines.toString().trim().split("\n").map(function(query) {
-        console.info("----------------------");
-        console.info(pretty.prettyPrint(query, width, grammar));
+        if (query.indexOf("”") > -1) {
+            return
+        }
+        runs_count += 1;
+        try {
+            var output = pretty.prettyPrint(query, width, grammar)
+        } catch (err) {
+            failures_count += 1;
+            console.info("----------------------");
+            console.info("Input:", query)
+            console.info("Failure:", err)
+            return;
+        }
+        if (!ONLY_ERRORS) {
+            console.info("----------------------");
+            console.info(output);
+        }
     });
+    console.info("======================");
+    console.info(failures_count + " failures out of " + runs_count + " runs");
 }
